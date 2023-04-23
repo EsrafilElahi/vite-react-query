f913faec65e90b41c4b55dcd1780e7ebd902bad5
esrafilelahi <essinho1121@gmail.com>
2023-04-23 16:12:59 +0330

    add tailwind css

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const ProductItem = ({ product }) => {
   return (
     <div>
-      <h2>title :{product.title}</h2>
+      <h2 className='text-3xl text-orange-900 font-bold underline'>title :{product.title}</h2>
       <h4>description :{product.desc}</h4>
-      <span>price :{product.price}</span>
+      <span className='text-3xl text-orange-900'>price :{product.price}</span>
     </div>)
 }
 
